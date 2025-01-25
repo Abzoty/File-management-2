e90342ba28a8eedf5320ca30cec125649e856e12
Abzoty <150899983+Abzoty@users.noreply.github.com>
2025-01-25 22:41:03 +0200

    still needs the split in insert
    fixed the look and structure , and the "type" and "next free" update

--- file.cpp
+++ file.cpp
@@ -3,34 +3,34 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
+using namespace std;
 
-const int MAX_RECORDS = 12; // Total slots including node type and next free node
+const int MAX_RECORDS = 11; // Total slots including node type and next free node
 const int MAX_KEYS = 5;     // m in the document
 
 struct Node
 {
     int nodeType;                 // 0 for leaf, 1 for non-leaf
-    int nextFreeNode;             // Index of next free node
-    int records[MAX_RECORDS - 2]; // Record/Reference pairs and child node indices
+    int records[MAX_RECORDS - 1]; // Record/Reference pairs and child node indices
 
-    Node() : nodeType(0), nextFreeNode(-1)
+    Node() : nodeType(-1)
     {
-        std::fill(std::begin(records), std::end(records), -1);
+        fill(begin(records), end(records), -1);
     }
 };
 
 void CreateIndexFileFile(const char *filename, int numberOfRecords, int m)
 {
-    std::ofstream file(filename, std::ios::binary);
+    ofstream file(filename, ios::binary);
 
     Node firstNode;
-    firstNode.nextFreeNode = 1;
+    firstNode.records[0] = 1;
     file.write(reinterpret_cast<const char *>(&firstNode), sizeof(Node));
 
     for (int i = 1; i < numberOfRecords; ++i)
     {
         Node emptyNode;
-        emptyNode.nextFreeNode = (i < numberOfRecords - 1) ? (i + 1) : -1;
+        emptyNode.records[0] = (i < numberOfRecords - 1) ? (i + 1) : -1;
         file.write(reinterpret_cast<const char *>(&emptyNode), sizeof(Node));
     }
 
@@ -39,7 +39,7 @@ void CreateIndexFileFile(const char *filename, int numberOfRecords, int m)
 
 Node readNode(const char *filename, int nodeIndex)
 {
-    std::ifstream file(filename, std::ios::binary);
+    ifstream file(filename, ios::in | ios::out | ios::binary);
     Node node;
     file.seekg(nodeIndex * sizeof(Node));
     file.read(reinterpret_cast<char *>(&node), sizeof(Node));
@@ -49,7 +49,7 @@ Node readNode(const char *filename, int nodeIndex)
 
 void writeNode(const char *filename, int nodeIndex, const Node &node)
 {
-    std::fstream file(filename, std::ios::in | std::ios::out | std::ios::binary);
+    fstream file(filename, ios::in | ios::out | ios::binary);
     file.seekp(nodeIndex * sizeof(Node));
     file.write(reinterpret_cast<const char *>(&node), sizeof(Node));
     file.close();
@@ -58,6 +58,12 @@ void writeNode(const char *filename, int nodeIndex, const Node &node)
 int findInsertPosition(const Node &node, int recordID)
 {
     int pos = 0;
+
+    if (node.nodeType == -1)
+    {
+        return pos;
+    }
+
     while (pos < MAX_RECORDS - 2 && node.records[pos] != -1 && node.records[pos] < recordID)
     {
         pos += 2;
@@ -65,7 +71,7 @@ int findInsertPosition(const Node &node, int recordID)
     return pos;
 }
 
-void splitChild(Node &x, Node &y)
+/* void splitChild(Node &x, Node &y)
 {
     int medianIndex = (MAX_RECORDS - 2) / 2;
 
@@ -90,47 +96,87 @@ void splitChild(Node &x, Node &y)
     // Update the nextFreeNode of the original node
     int newNodeIndex = x.nextFreeNode;
     x.nextFreeNode = newNodeIndex;
+} */
+
+void updateHead()
+{
+    int i = 1;
+    while (true)
+    {
+        Node head = readNode("index.bin", i);
+        if (head.nodeType == -1)
+            break;
+
+        i++;
+    }
+
+    Node head = readNode("index.bin", 0);
+    head.records[0] = i;
+    writeNode("index.bin", 0, head);
 }
 
-int InsertNewRecordAtIndex(const char *filename, int recordID, int reference)
+int Insert(const char *filename, int nodeIndex, int recordID, int reference)
 {
-    Node rootNode = readNode(filename, 1);
-    if (rootNode.records[MAX_RECORDS - 3] != -1)
+    Node rootNode = readNode(filename, nodeIndex);
+
+    if (rootNode.nodeType == 0 || rootNode.nodeType == -1)
     {
-        // Node is full, perform a split
-        Node newNode;
-        newNode.nodeType = 1; // Non-leaf node
-        newNode.nextFreeNode = -1;
+        // Node is a leaf, find the right position to insert
+        int pos = findInsertPosition(rootNode, recordID);
 
-        // Split the node
-        splitChild(rootNode, newNode);
+        if (rootNode.nodeType != -1)
+        {
+            for (int i = MAX_RECORDS - 2; i > pos; i -= 2)
+            {
+                rootNode.records[i] = rootNode.records[i - 2];
+                rootNode.records[i + 1] = rootNode.records[i - 1];
+            }
+        }
 
-        // Write the updated nodes to the file
-        int newNodeIndex = rootNode.nextFreeNode;
-        writeNode(filename, 1, rootNode);
-        writeNode(filename, newNodeIndex, newNode);
+        rootNode.records[pos] = recordID;
+        rootNode.records[pos + 1] = reference;
+        int type = rootNode.nodeType;
+        rootNode.nodeType = 0;
+        writeNode(filename, nodeIndex, rootNode);
 
-        // Recursively insert the record into the new node
-        return InsertNewRecordAtIndex(filename, recordID, reference);
+        if (type == -1)
+        {
+            updateHead();
+        }
+
+        return nodeIndex;
     }
-
-    // Node is not full, insert the record as usual
-    int pos = findInsertPosition(rootNode, recordID);
-
-    for (int i = MAX_RECORDS - 3; i > pos; i -= 2)
+    else if (rootNode.nodeType == 1)
     {
-        rootNode.records[i] = rootNode.records[i - 2];
-        rootNode.records[i + 1] = rootNode.records[i - 1];
-    }
+        int childIndex = -1, i = 0;
+        // Node is non-leaf, find the first item bigger than recordID
+        while (rootNode.records[i] != -1)
+        {
+            if (rootNode.records[i] > recordID)
+            {
+                childIndex = rootNode.records[i + 1]; // Return the reference (index + 1)
+            }
+            i += 2;
+        }
 
-    rootNode.records[pos] = recordID;
-    rootNode.records[pos + 1] = reference;
-    writeNode(filename, 1, rootNode);
+        if (childIndex == -1)
+        {
+            // No child found, insert the record in the last child
+            childIndex = rootNode.records[i - 1];
+        }
+
+        return Insert(filename, childIndex, recordID, reference);
+    }
 
     return 1;
 }
 
-void DeleteRecordFromIndex(const char *filename, int recordID)
+int InsertNewRecordAtIndex(const char *filename, int recordID, int reference)
+{
+    return Insert(filename, 1, recordID, reference);
+}
+
+/* void DeleteRecordFromIndex(const char *filename, int recordID)
 {
     Node rootNode = readNode(filename, 1);
     for (int i = 0; i < MAX_RECORDS - 2; i += 2)
@@ -163,23 +209,23 @@ int SearchARecord(const char *filename, int recordID)
         }
     }
     return -1; // Not found
-}
+} */
 
 void DisplayIndexFileContent(const char *filename)
 {
-    std::ifstream file(filename, std::ios::binary);
+    ifstream file(filename, ios::binary);
     int nodeCount = 0;
     Node node;
 
     while (file.read(reinterpret_cast<char *>(&node), sizeof(Node)))
     {
-        std::cout << "Node " << std::setw(2) << nodeCount << ": ";
-        std::cout << "Type: " << std::setw(2) << node.nodeType << ", Next Free: " << std::setw(2) << node.nextFreeNode << " | ";
-        for (int i = 0; i < MAX_RECORDS - 2; ++i)
+        cout << "Node " << setw(2) << nodeCount << ": ";
+        cout << "Type: " << setw(2) << node.nodeType << " | ";
+        for (int i = 0; i < MAX_RECORDS - 1; ++i)
         {
-            std::cout << std::setw(2) << node.records[i] << " ";
+            cout << setw(2) << node.records[i] << " ";
         }
-        std::cout << std::endl;
+        cout << endl;
         nodeCount++;
     }
 
@@ -190,6 +236,8 @@ int main()
 {
     const char *filename = "index.bin";
     CreateIndexFileFile(filename, 10, 5);
+    cout << "Index File Content After Creation:" << endl;
+    DisplayIndexFileContent(filename);
 
     InsertNewRecordAtIndex(filename, 3, 12);
     InsertNewRecordAtIndex(filename, 7, 24);
@@ -197,24 +245,24 @@ int main()
     InsertNewRecordAtIndex(filename, 24, 60);
     InsertNewRecordAtIndex(filename, 14, 72);
 
-    std::cout << "Index File Content After Insertions:" << std::endl;
+    cout << "Index File Content After Insertions:" << endl;
     DisplayIndexFileContent(filename);
 
-    InsertNewRecordAtIndex(filename, 19, 84);
+    // InsertNewRecordAtIndex(filename, 19, 84);
 
-    std::cout << "Index File Content After Split:" << std::endl;
-    DisplayIndexFileContent(filename);
+    // cout << "Index File Content After Split:" << endl;
+    // DisplayIndexFileContent(filename);
 
     // DeleteRecordFromIndex(filename, 10);
 
-    // std::cout << "Index File Content After Deletion:" << std::endl;
+    // cout << "Index File Content After Deletion:" << endl;
     // DisplayIndexFileContent(filename);
 
     // int ref = SearchARecord(filename, 7);
     // if (ref != -1)
-    //     std::cout << "Record 7 found with reference: " << ref << std::endl;
+    //     cout << "Record 7 found with reference: " << ref << endl;
     // else
-    //     std::cout << "Record 7 not found." << std::endl;
+    //     cout << "Record 7 not found." << endl;
 
     return 0;
 }
